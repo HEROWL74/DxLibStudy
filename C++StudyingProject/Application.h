300a3a1c2d62cb09ffb8bd820107868d448c0e97
HEROWL <hiroakitogawa74@gmail.com>
2025-05-22 19:00:42 +0900

    Application.h追加（operator使用）

---
+++ C++StudyingProject/Application.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "DxLib.h"
+#include <memory>
+class Application
+{
+public:
+	Application();
+	~Application();
+
+	//ÉRÉsÅ[Çã÷é~Ç≥ÇπÇÈ
+	Application(const Application&) = delete;
+	Application& operator=(const Application&) = delete;
+
+	void Run();
+
+private:
+	bool Initialize();
+	void Update();
+	void Release();
+};
