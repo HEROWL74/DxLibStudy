5578a82f8f1b9bcb162efcd42d02dc1962b36cc4
HEROWL <hiroakitogawa74@gmail.com>
2025-05-27 10:38:59 +0900

    Coin獲得エフェクト

--- C++StudyingProject/Coin.cpp
+++ C++StudyingProject/Coin.cpp
@@ -1,7 +1,14 @@
 #include "Coin.h"
 
-Coin::Coin(int x, int y)
+Coin::Coin(int x, int y, int tx, int ty)
     : m_x(x), m_y(y), m_collected(false)
+    , m_startX((float)x)
+    , m_startY((float)y)
+    , m_targetX(tx)
+    , m_targetY(ty)
+    , m_timer(0)
+    , m_duration(30)     // 約30フレームで飛ぶ
+    , m_state(State::Idle)
 {
     m_handle = LoadGraph("Sprites/Tiles/Default/hud_coin.png");
     GetGraphSize(m_handle, &m_w, &m_h);
@@ -19,18 +26,38 @@ void Coin::Update(Player& player) {
     int pw = player.GetW();
     int ph = player.GetH();
 
-    // AABB 衝突判定
-    if (!(px + pw  < m_x ||
-        px      > m_x + m_w ||
-        py + ph  < m_y ||
-        py      > m_y + m_h))
-    {
-        m_collected = true;
-        player.AddCoin(1);  // プレイヤーのコイン数を +1
+    if (m_state == State::Idle) {
+        // AABB 衝突判定
+        if (!(px + pw  < m_x ||
+            px      > m_x + m_w ||
+            py + ph  < m_y ||
+            py      > m_y + m_h))
+        {
+            m_collected = true;
+            player.AddCoin(1);  // プレイヤーのコイン数を +1
+            // 回収スタート
+            m_state = State::MovingToHUD;
+            m_startX = m_x;
+            m_startY = m_y;
+            m_timer = 0;
+        }
     }
+    else if (m_state == State::MovingToHUD) {
+        // t を [0,1] で進行
+        float t = (float)++m_timer / m_duration;
+        if (t >= 1.0f) {
+            t = 1.0f;
+            m_state = State::Collected;
+            player.AddCoin(1);  // 到着時にコイン数を+1
+        }
+        // イージングなしの線形補間
+        m_x = m_startX + (m_targetX - m_startX) * t;
+        m_y = m_startY + (m_targetY - m_startY) * t;
+    }
+
 }
 
 void Coin::Draw() const {
-    if (m_collected) return;
-    DrawGraph(m_x, m_y, m_handle, TRUE);
+    if (m_state == State::Collected) return;
+    DrawGraph((int)m_x, (int)m_y, m_handle, TRUE);
 }
--- C++StudyingProject/Coin.h
+++ C++StudyingProject/Coin.h
@@ -4,19 +4,34 @@
 
 class Coin {
 public:
-    Coin(int x, int y);
+    Coin(int x, int y, int tx, int ty);
     ~Coin();
 
     // 毎フレーム更新。回収されたら isCollected() が true になる
     void Update(Player& player);
     void Draw() const;
 
+    bool IsDone() const { return m_state == State::Collected; }
+
     bool isCollected() const { return m_collected; }
 
 private:
-    int m_x, m_y;
+
+    enum class State { Idle, MovingToHUD, Collected };
+    float m_x, m_y;
     int m_w, m_h;
     int m_handle;
 
+    // アニメーション開始座標
+    float m_startX, m_startY;
+    // HUDアイコン座標
+    int   m_targetX, m_targetY;
+    // アニメーション進行
+    int   m_timer, m_duration;
+
+    State m_state;
+
     bool m_collected;
+
+    static constexpr int COLLIDE_MARGIN = 0;
 };
